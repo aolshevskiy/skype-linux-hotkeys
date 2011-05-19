f5287c4eaf1746c94b989dee8d970d10d1199645
Artyom Olshevskiy <siasiamail@gmail.com>
2011-05-19 20:53:44 +0300

    Added call center support

--- skype-hotkeys.py
+++ skype-hotkeys.py
@@ -5,9 +5,28 @@ skype = bus.get_object('com.Skype.API', '/com/Skype')
 skype.Invoke('NAME linux-hotkeys')
 skype.Invoke('PROTOCOL 5')
 import sys
-command = " ".join(sys.argv[1:])
-if(command == '-m'):
+args = sys.argv[1:]
+command = args[0]
+if command == '-m':
     command = "MUTE ON"
     if skype.Invoke('GET MUTE') == "MUTE ON":
         command = "MUTE OFF"
     skype.Invoke(command)
+elif command == '-c':
+    import ConfigParser
+    config = ConfigParser.ConfigParser()
+    from os.path import join, expanduser
+    config.read(join(expanduser('~'), 'bin', 'skype-hotkeys.cfg'))
+    (number, code) = config.get('skype-hotkeys', ' '.join(args[1:])).split()
+    res = skype.Invoke('CALL ' + number).split()
+    call_id = res[1]
+    import time
+    status = res[3]
+    while status != 'INPROGRESS':
+        time.sleep(1)
+        res = skype.Invoke('GET CALL ' + call_id + ' STATUS').split()
+        status = res[3]
+    time.sleep(1)
+    for c in code:
+        time.sleep(0.4)
+        skype.Invoke('SET CALL ' + call_id + ' DTMF ' + c)
